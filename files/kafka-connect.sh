dd1af280c5df062a74ea87d32c2370322a48671e
Luis David Barrios <luisdavid.barrios@agsnasoft.com>
2017-03-24 16:43:10 +0100

    Multiple connectors are supported in same execution

--- files/kafka-connect.sh
+++ files/kafka-connect.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # Common configuration
@@ -6,7 +6,7 @@ set -e
 # Functions
 
 use() {
-    echo "kafka-connect.sh name|file connector_cfg [server.cfg]"
+    echo "kafka-connect.sh [--servercfg server.cfg] name name_1|file connector_cfg_1 [name name_2|file connector_cfg_2 .. name name_n|file connector_cfg_n] "
     echo "  name : set that connector_cfg is a name that will be used to load"
     echo "    configuration from /etc/kafka-connect/connector_cfg.properties"
     echo "  file : set that connector_cfg is a path to a file with connector config"
@@ -17,35 +17,47 @@ use() {
 
 
 start_server() {
-  connect-standalone.sh "$1" "$2"
+  connect-standalone.sh $@
 }
 
 # Main
 
-if [ -z "$2" ]
+if [ -z "$1" ]
 then
   use
   exit 1
 fi
 
-connector_cfg=""
-case $1 in
-  name)
-    connector_cfg="/etc/kafka-connect/${2}.properties"
-    ;;
-  file)
-    connector_cfg="$2"
-    ;;
-  *)
-    use
-    exit 1
-    ;;
-esac
-
+# Default config
 server_cfg_file="/etc/kafka-connect/connect.properties"
-if [ ! -z "$3" ]
-then
-  server_cfg_file="$3"
-fi
 
-start_server "${server_cfg_file}" "${connector_cfg}"
+connectors_cfg=()
+while [ -n "$1" ]; do
+  case $1 in
+    --server)
+      if [ "$2" == "" ]
+      then
+        use
+        exit 1
+      else
+        server_cfg_file="$2"
+        shift
+      fi
+      ;;
+    name)
+      connectors_cfg+=("/etc/kafka-connect/${2}.properties")
+      shift
+      ;;
+    file)
+      connectors_cfg+=("$2")
+      shift
+      ;;
+    *)
+      use
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+start_server "${server_cfg_file}" ${connectors_cfg[@]}
