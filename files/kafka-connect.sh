457d7fc402a69ca3046a5a238441ff768853cea4
Santiago Saavedra <ssaavedra@openshine.com>
2017-05-05 19:00:19 +0200

    Allow overriding environment variables and add env expansion
    Now properties files support the `${VARIABLE}` and even the
    `${VARIABLE:-default-value}` syntax so that they can be replaced by
    environment variables in production settings.
    
    The kafka-connect.sh launcher also supports replacing variables that
    are already in the properties file by using the following convention:
    
    KC_OVERRIDE_CONNECTION_URL=http://elastic.svc.cluster.local:80
    
    will replace all occurrences of the connection.url variable for
    
    connection.url=http://elastic.svc.cluster.local:80
    
    That is, all variables are transformed to lowercase and underscores to
    dots.

--- files/kafka-connect.sh
+++ files/kafka-connect.sh
@@ -20,6 +20,43 @@ start_server() {
   connect-standalone.sh $@
 }
 
+expandVarsStrict() {
+    # Originally from http://stackoverflow.com/a/40167919/488191
+    # Excerpt under CC-By-SA 3.0 by http://stackoverflow.com/users/45375/
+
+    local line lineEscaped
+    while IFS= read -r line || [[ -n $line ]]; do # the `||` clause ensures that the last line is read even if it doesn't end with \n
+	# Escape ALL chars. that could trigger an expansion
+	IFS= read -r -d '' lineEscaped < <(printf %s "$line" | tr '`([$' '\1\2\3\4')
+	# ... then selectively reenable ${ references
+	lineEscaped=${lineEscaped//$'\4{'/\$'{'}
+	# Finally, escape embedded double quotes to preserve them.
+	lineEscaped=${lineEscaped//\"/\\\"}
+	eval "printf '%s\n' \"$lineEscaped\"" | tr '\1\2\3\4' '`([$'
+    done
+}
+
+resolve_variables() {
+    for VAR in `env`
+    do
+	if [[ $VAR =~ ^KC_OVERRIDE_ ]]; then
+	    kconnect_name=$(echo "$VAR" | sed -r 's/KC_OVERRIDE_(.*)=.*/\1/g' | tr '[:upper:]' '[:lower:]' | tr _ .)
+	    env_var=$(echo "$VAR" | sed -r 's/(.*)=.*/\1/g')
+
+	    if egrep -q "(^|^#)$kconnect_name=" "$1"; then
+		sed -r -i "s@(^|^#)($kconnect_name)=(.*)@\2=${!env_var}@g" "$1"
+	    else
+		echo "Preexisting variable $kconnect_name not found at $1" >&2
+	    fi
+	fi
+
+	expandVarsStrict < "$1" > "$1.exp.properties"
+
+	echo "$1.exp.properties"
+    done
+}
+
+
 # Main
 
 if [ -z "$1" ]
@@ -45,11 +82,11 @@ while [ -n "$1" ]; do
       fi
       ;;
     name)
-      connectors_cfg+=("/etc/kafka-connect/${2}.properties")
+      connectors_cfg+=($(resolve_variables "/etc/kafka-connect/${2}.properties"))
       shift
       ;;
     file)
-      connectors_cfg+=("$2")
+      connectors_cfg+=($(resolve_variables "$2"))
       shift
       ;;
     *)
